078081429c0e57b9cc48a81ec7ed5232e992adb9
Jari Haavisto <jari.haavisto@gmail.com>
2020-09-20 15:56:08 +0300

    add methods and glasses endpoints

--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const passport = require('passport')
 
-const {authenticate, isAdmin} = require('../auth/authUtils')
+const { authenticate, isAdmin } = require('../auth/authUtils')
 
 require('../auth/passport')(passport)
 router.use(passport.initialize())
@@ -9,15 +9,14 @@ router.use(passport.initialize())
 router.use('/users', require('./userRouter'))
 router.use('/cocktails', require('./cocktailRouter'))
 
+router.get('/methods', authenticate, async (req, res) => {
+    res.json(['Shaken', 'Stirred', 'Flash Blended', 'Built'])
+})
+router.get('/glasses', authenticate, isAdmin, async (req, res) => {
+    res.json(['Double Old Fashioned', 'Hurricane', 'Coupe'])
+})
 router.get('/', (req, res) => {
     res.send('Cocktail Index Server')
-})
-
-router.get('/auth', authenticate, async (req, res) => {
-    res.send('Authenticated endpoint')
-})
-router.get('/admin', authenticate, isAdmin, async (req, res) => {
-    res.send('Admin endpoint')
 })
 
 module.exports = router
