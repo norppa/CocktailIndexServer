fdffd47f47692a706f9a310ecd3fecdef78caf0a
Jari Haavisto <jari.haavisto@gmail.com>
2020-09-22 22:04:21 +0300

    glasses endopoint does not require admin privileges

--- routers/router.js
+++ routers/router.js
@@ -12,7 +12,7 @@ router.use('/cocktails', require('./cocktailRouter'))
 router.get('/methods', authenticate, async (req, res) => {
     res.json(['Shaken', 'Stirred', 'Flash Blended', 'Built'])
 })
-router.get('/glasses', authenticate, isAdmin, async (req, res) => {
+router.get('/glasses', authenticate, async (req, res) => {
     res.json(['Double Old Fashioned', 'Hurricane', 'Coupe'])
 })
 router.get('/', (req, res) => {
